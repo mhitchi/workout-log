63b995d6f3626778e505453b99982f1eabe659ed
mhitchi <mayhitchings@gmail.com>
2020-02-21 09:47:06 -0500

    added wildcard id to put route, workout adding successfully

--- server.js
+++ server.js
@@ -28,17 +28,49 @@ app.get("/exercise", (req, res) => {
   res.sendFile(`${__dirname}/public/exercise.html`);
 });
 
+// app.get(`/exercise?id=${id}`, (req, res) => {
+
+// });
+
 //add workouts to plan
 //POST `/api/workouts/${id}`
 //TODO get id
-app.post("/api/workouts", ({body}, res) => {
-  db.Workout.create({body})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+app.put("/api/workouts/:id?", (req, res) => {
+  //if cardio, get name, duration, distance
+  if( req.body.type === "cardio" ) {
+    db.Workout.create(
+      {
+        name: req.body.name,
+        type: req.body.type,
+        duration: req.body.duration,
+        distance: req.body.distance,
+      }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+    //if resistance, get name, weight, sets, reps, duration
+  } else {
+    db.Workout.create(
+      {
+        name: req.body.name,
+        type: req.body.type,
+        weight: req.body.weight,
+        sets: req.body.sets,
+        reps: req.body.reps,
+        duration: req.body.resistance-duration,
+      }
+    )
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  }
 });
 
 //add new workouts to new plan
